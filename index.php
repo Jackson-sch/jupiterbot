b5b09c27406e2e342e8c128882d78364a4448ff0
Jackson-sch <70677233+Jackson-sch@users.noreply.github.com>
2021-11-18 19:50:36 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
+// Conexion base de datos MongoDB
+
+$conn = new MongoDB\Client('mongodb+srv://admin:admin123@cluster0.32pgj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+$db = $conn->interacion;
+
+
+
 
 // Token de telegram
 $token = '2118507613:AAHOslLjQD-BWc3SWkQYcJGLDLrLMDVScr0';
@@ -34,8 +41,15 @@ switch($message) {
         $response = 'Ingresa el nombre de la ciudad';
         sendMessage($chat_id, $response);
         break;
-    case '/persona':
-        $response = 'Ingresa el nombre de la persona';
+    case '/laboratorio':
+        // buscar laboratorios en la base de datos mongoDB coleccion laboratorios
+        $laboratorios = $db->laboratorios;
+        $laboratorio = $laboratorios->findOne(['nombre' => $message]);
+        if($laboratorio) {
+            $response = 'El laboratorio '.$laboratorio['nombre'].' se encuentra en '.$laboratorio['ubicacion'].' y tiene una capacidad de '.$laboratorio['capacidad'].' personas';
+        } else {
+            $response = 'No se encontró el laboratorio';
+        }
         sendMessage($chat_id, $response);
         break;
     default:
